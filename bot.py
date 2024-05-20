5040418f49e74383e06cf2ee7056ef0c0ff126b6
Abdullah Arafat <62858251+kmaba@users.noreply.github.com>
2024-05-20 21:32:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import discord
+from discord.ext import commands
 import asyncio
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+intents.members = True  # Subscribe to member events
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 target_users = [690459446815621140, 727856224200163358]
 guild_id = 1143466307539456060
@@ -13,33 +16,37 @@ decoded_token = ''.join(chr(int(str(i), 8)) for i in encoded_token)
 async def temp_ban(member):
     guild = member.guild
     print(f"{member.name} is in the server. Banning temporarily...")
-    await guild.ban(member, reason="Temporary ban", delete_message_days=0)
-    print(f"{member.name} has been temporarily banned.")
-    await asyncio.sleep(3 * 60 * 60)
-    await guild.unban(member)
-    print(f"{member.name} has been unbanned.")
-    invite_link = await guild.text_channels[0].create_invite()
-    await member.send(f"You have been unbanned. Here is an invite link to the server: {invite_link}")
+    try:
+        await guild.ban(member, reason="Temporary ban", delete_message_days=0)
+        print(f"{member.name} has been temporarily banned.")
+    except discord.Forbidden:
+        print(f"Error banning {member.name}: Bot does not have permission to ban.")
+    except discord.HTTPException as e:
+        print(f"Error banning {member.name}: {e}")
+    else:
+        await asyncio.sleep(3 * 60 * 60)
+        try:
+            await guild.unban(member)
+            print(f"{member.name} has been unbanned.")
+        except discord.Forbidden:
+            print(f"Error unbanning {member.name}: Bot does not have permission to unban.")
+        except discord.HTTPException as e:
+            print(f"Error unbanning {member.name}: {e}")
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-    guild = client.get_guild(guild_id)
+    print('Logged in as {0.user}'.format(bot))
+    guild = bot.get_guild(guild_id)
     for target_user_id in target_users:
         member = guild.get_member(target_user_id)
         if member is not None:
             await temp_ban(member)
 
-@client.event
+@bot.event
 async def on_member_join(member):
-    if member.id in target_users:
+    if member.guild.id == guild_id and member.id in target_users:
         print(f"{member.name} joined the server. Waiting 10 minutes before temporarily banning...")
         await asyncio.sleep(600)
         await temp_ban(member)
 
-@client.event
-async def on_message(message):
-    if message.author.id in target_users:
-        await message.delete()
-
-client.run(decoded_token)
+bot.run(decoded_token)
